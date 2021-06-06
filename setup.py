92e4c0649773e9faff4901301ccd2f844768297a
ParsaAlizadeh <parsa.alizadeh1@gmail.com>
2021-06-06 11:54:45 +0430

    Add requirements file

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
-from upt import __name__, __version__
+from upt import __version__
 
 with open('README.md') as readme_file:
     README = readme_file.read()
 
+with open('requirements.txt') as req_file:
+    requirements = req_file.readlines()
+
 setup(
-    name=__name__,
+    name='universal-parser-tool',
     version=__version__,
     description='Useful tool to speedup testing codes in cp-programming',
     license='GPL',
@@ -15,12 +18,7 @@ setup(
     long_description_content_type="text/markdown",
     long_description=README + '\n',
     packages=find_packages(include=['upt', 'upt.*']),
-    install_requires=[
-        'selenium',
-        'requests',
-        'bs4',
-        'markdown',
-    ],
+    install_requires=requirements,
     python_requires='>=3.6',
     entry_points={
         'console_scripts': ['upt=upt:main']
